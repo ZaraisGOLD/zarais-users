21c8690806c8884a93a8ac085d94f2d5fd2c47de
ZaraisGOLD <zaraisgold@gmail.com>
2023-04-24 19:01:51 -0400

    8

--- src/components/FormUser.jsx
+++ src/components/FormUser.jsx
@@ -5,100 +5,94 @@ import './styles/formUser.css'
 
 const FormUser = ({ postUser, updateInfo, updateUser, setUpdateInfo, setFormClose, formClose, setConfirm, confirm }) => {
 
-    const { register, handleSubmit, reset } = useForm()
+  const { register, handleSubmit, reset } = useForm()
 
-    // const msgSuccessful = `{updateInfo ? 'Update' : 'Create'}successful`
+  useEffect(() => {
+    reset(updateInfo)
+  }, [updateInfo])
 
-    useEffect(() => {
-        reset(updateInfo)
-    }, [updateInfo])
-
-    const submit = data => {
-        if (updateInfo) {
-            updateUser(updateInfo.id, data)
-            setUpdateInfo()
-            setFormClose(true)
-            setConfirm(false)
-        } else {
-            postUser(data)
-            setFormClose(true)
-            setConfirm(false)
-        }
-        reset(defaultValues)
+  const submit = data => {
+    if (updateInfo) {
+      updateUser(updateInfo.id, data)
+      setUpdateInfo()
+      setFormClose(true)
+      setConfirm(false)
+    } else {
+      postUser(data)
+      setFormClose(true)
+      setConfirm(false)
     }
+    reset(defaultValues)
+  }
 
-    const handleFormClose = () => {
-        reset(defaultValues)
-        setUpdateInfo()
-        setFormClose(true)
-    }
+  const handleFormClose = () => {
+    reset(defaultValues)
+    setUpdateInfo()
+    setFormClose(true)
+  }
 
-    const handleConfirm = e => {
-        e.preventDefault()
-        setConfirm(true)
-    }
+  const handleConfirm = e => {
+    e.preventDefault()
+    setConfirm(true)
+  }
 
-    const handleConfirmClose = () => {
-        setConfirm(false)
-    }
+  const handleConfirmClose = () => {
+    setConfirm(false)
+  }
 
-
-    return (
-        <div className={`form__container ${formClose ? 'form__container--close' : ''}`}>
-            <form className='form' onSubmit={handleConfirm}>
-                <h3 className='form__title'>{updateInfo ? 'Update User' : 'Create New User'}</h3>
-                <i onClick={handleFormClose} className='bx bx-x'></i>
-                <div className='form__item'>
-                    <label className='form__label' htmlFor="email">Email
-                        <span className='form__label--req'> *</span>
-                    </label>
-                    <input className='form__input' {...register('email')} type="email" id='email' placeholder='e.g: email@example.com' required />
-                </div>
-                <div className='form__item'>
-                    <label className='form__label' htmlFor="password">Password
-                        <span className='form__label--req'> *</span>
-                    </label>
-                    <input className='form__input' {...register('password')} type="password" id='password' placeholder='e.g: User2000*' pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[#$%&*_=+-]).{8,16}$" required />
-                </div>
-                <div className='form__item'>
-                    <label className='form__label' htmlFor="first_name">First Name
-                        <span className='form__label--req'> *</span>
-                    </label>
-                    <input className='form__input' {...register('first_name')} type="text" id='first_name' placeholder='e.g: John' required />
-                </div>
-                <div className='form__item'>
-                    <label className='form__label' htmlFor="last_name">Last Name
-                        <span className='form__label--req'> *</span>
-                    </label>
-                    <input className='form__input' {...register('last_name')} type="text" id='last_name' placeholder='e.g: Smith' required />
-                </div>
-                <div className='form__item'>
-                    <label className='form__label' htmlFor="birthday">Birthday
-                        <span className='form__label--req'> *</span>
-                    </label>
-                    <input className='form__input' {...register('birthday')} type="date" id='birthday' required />
-                </div>
-                <button className='form__btn'>{updateInfo ? 'Update' : 'Create'}</button>
-                <p className='form__req'>
-                    <span className='form__req--sym'> *</span>All fields are required
-                </p>
-            </form>
-            {confirm && (
-                <div className='form__confirm--container' >
-                    <div className='form__confirm'>
-                        <p className='form__confirm--alert'>Are you sure want to {updateInfo ? 'update' : 'create'}?</p>
-                        <div className='form__confirm--icons'>
-                            <i onClick={handleConfirmClose} className='bx bx-x-circle'></i>
-                            <i onClick={handleSubmit(submit)} className='bx bx-check-circle'></i>
-                        </div>
-                    </div>
-                </div>
-            )}
-            {/* {msgSuccessful && 
-            <div>{updateInfo ? 'Update' : 'Create'} successful</div>
-            } */}
+  return (
+    <div className={`form__container ${formClose ? 'form__container--close' : ''}`}>
+      <form className='form' onSubmit={handleConfirm}>
+        <h3 className='form__title'>{updateInfo ? 'Update User' : 'Create New User'}</h3>
+        <i onClick={handleFormClose} className='bx bx-x'></i>
+        <div className='form__item'>
+          <label className='form__label' htmlFor="email">Email
+            <span className='form__label--req'> *</span>
+          </label>
+          <input className='form__input' {...register('email')} type="email" id='email' placeholder='e.g: email@example.com' required />
         </div>
-    )
+        <div className='form__item'>
+          <label className='form__label' htmlFor="password">Password
+            <span className='form__label--req'> *</span>
+          </label>
+          <input className='form__input' {...register('password')} type="password" id='password' placeholder='e.g: User2000*' pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[#$%&*_=+-]).{8,16}$" required />
+        </div>
+        <div className='form__item'>
+          <label className='form__label' htmlFor="first_name">First Name
+            <span className='form__label--req'> *</span>
+          </label>
+          <input className='form__input' {...register('first_name')} type="text" id='first_name' placeholder='e.g: John' required />
+        </div>
+        <div className='form__item'>
+          <label className='form__label' htmlFor="last_name">Last Name
+            <span className='form__label--req'> *</span>
+          </label>
+          <input className='form__input' {...register('last_name')} type="text" id='last_name' placeholder='e.g: Smith' required />
+        </div>
+        <div className='form__item'>
+          <label className='form__label' htmlFor="birthday">Birthday
+            <span className='form__label--req'> *</span>
+          </label>
+          <input className='form__input' {...register('birthday')} type="date" id='birthday' required />
+        </div>
+        <button className='form__btn'>{updateInfo ? 'Update' : 'Create'}</button>
+        <p className='form__req'>
+          <span className='form__req--sym'> *</span>All fields are required
+        </p>
+      </form>
+      {confirm && (
+        <div className='form__confirm--container' >
+          <div className='form__confirm'>
+            <p className='form__confirm--alert'>Are you sure want to {updateInfo ? 'update' : 'create'}?</p>
+            <div className='form__confirm--icons'>
+              <i onClick={handleConfirmClose} className='bx bx-x-circle'></i>
+              <i onClick={handleSubmit(submit)} className='bx bx-check-circle'></i>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default FormUser
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -13,26 +13,35 @@ const UserCard = ({ user, deleteUser, setUpdateInfo, setFormClose, setConfirmDel
     setFormClose(false)
   }
 
-  const handleConfirmDelete = e => {
+  const handleConfirmDelete = (e) => {
     e.preventDefault()
     setConfirmDelete(true)
   }
 
-  const handleConfirmDeleteClose = () => {
+  const handleDeleteClose = () => {
     setConfirmDelete(false)
   }
 
   return (
     <div className='user'>
-      <h2 className='user__name'>{user.first_name} {user.last_name}</h2>
+      <div className='user__nameIcon'>
+        <i className='bx bx-user'></i>
+        <h2 className='user__name'>{user.first_name} {user.last_name}</h2>
+      </div>
       <ul className='user__list'>
         <li className='user__item'>
           <span className='user__label'>Email: </span>
-          <span className='user__value'>{user.email}</span>
+          <div className='user__info'>
+            <i className='bx bx-envelope'></i>
+            <span className='user__value'>{user.email}</span>
+          </div>
         </li>
         <li className='user__item'>
           <span className='user__label'>Birthday: </span>
-          <span className='user__value'>{user.birthday}</span>
+          <div className='user__info'>
+            <i className='bx bx-gift'></i>
+            <span className='user__value'>{user.birthday}</span>
+          </div>
         </li>
       </ul>
       <footer className='user__footer'>
@@ -42,18 +51,18 @@ const UserCard = ({ user, deleteUser, setUpdateInfo, setFormClose, setConfirmDel
         <button className='user__btn user__update' onClick={handleUpdate}>
           <i className='bx bx-edit user__icon'></i>
         </button>
-        {confirmDelete && (
-          <div className='form__confirm--container' >
-            <div className='form__confirm'>
-              <p className='form__confirm--alert'>Are you sure want to delete the user {user.first_name} {user.last_name}?</p>
-              <div className='form__confirm--icons'>
-                <i onClick={handleConfirmDeleteClose} className='bx bx-x-circle'></i>
-                <i onClick={handleDelete} className='bx bx-check-circle'></i>
-              </div>
+      </footer>
+      {confirmDelete && (
+        <div className='form__confirm--container' >
+          <div className='form__confirm'>
+            <p className='form__confirm--alert'>Are you sure want to delete?</p>
+            <div className='form__confirm--icons'>
+              <i onClick={handleDeleteClose} className='bx bx-x-circle'></i>
+              <i onClick={handleDelete} className='bx bx-check-circle'></i>
             </div>
           </div>
-        )}
-      </footer>
+        </div>
+      )}
     </div>
   )
 }
