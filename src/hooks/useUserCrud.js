ccb6403b90046699ecdca49d8a2413d9c4c967f3
ZaraisGOLD <zaraisgold@gmail.com>
2023-06-05 03:13:25 -0400

    join backend

--- src/hooks/useUserCrud.js
+++ src/hooks/useUserCrud.js
@@ -5,23 +5,23 @@ const useUserCrud = () => {
 
     const [users, setUsers] = useState()
 
-    const url = `https://users-crud.academlo.tech/users/`
+    const url = 'https://zarais-user-crud-academlo.onrender.com/api/v1'
 
     const getAlUsers = () => {
-        axios.get(url)
+        axios.get(`${url}/users`)
             .then(res => setUsers(res.data))
             .catch(err => console.log(err))
     }
 
     const postUser = data => {
-        axios.post(url, data)
+        axios.post(`${url}/users`, data)
             .then(() => getAlUsers())
             .catch(err => console.log(err))
     }
 
     const deleteUser = id => {
 
-        const urlDelete = `${url}${id}/`
+        const urlDelete = `${url}/users/${id}`
 
         axios.delete(urlDelete)
             .then(()=> getAlUsers())
@@ -30,7 +30,7 @@ const useUserCrud = () => {
 
     const updateUser = (id, data) => {
 
-        const urlUpdate = `${url}${id}/`
+        const urlUpdate = `${url}/users/${id}`
 
         axios.patch(urlUpdate, data)
             .then(() => getAlUsers())
